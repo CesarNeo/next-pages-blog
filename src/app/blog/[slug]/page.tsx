db40c504a0863dc20b6d3878da931ca09ba7bd14
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-05-31 14:48:27 -0300

    feat: generateStaticParams

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,6 +3,12 @@ import type { TBlogPostPageProps } from '@/templates/blog/post-page/types';
 import { allPosts } from 'contentlayer/generated';
 import { notFound } from 'next/navigation';
 
+export async function generateStaticParams() {
+  return allPosts.map(post => ({
+    slug: post.slug,
+  }));
+}
+
 async function PostPage({ params }: TBlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find(p => p.slug === slug);
