c6ff1b2dc31e27d5d98f70da96da972c28755926
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-05-31 15:07:24 -0300

    feat: SEO blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import BlogList from '@/templates/blog/blog-list';
 import { allPosts } from 'contentlayer/generated';
+import type { Metadata } from 'next';
+
+export const metadata = {
+  title: 'Blog | Site.Set',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog | Site.Set',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    url: 'https://next-pages-blog-ecru.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://next-pages-blog-ecru.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set - Venda seus produtos como afiliado em um único lugar',
+      },
+    ],
+  },
+} satisfies Metadata;
 
 function BlogPage() {
   const posts = allPosts.sort((a, b) => {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import Layout from '@/components/layout';
 import '@/styles/globals.css';
+import type { Metadata } from 'next';
 
 import type { PropsWithChildren } from 'react';
+
+export const metadata = {
+  title: 'Site.Set',
+  description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+} satisfies Metadata;
 
 function RootLayout({ children }: PropsWithChildren) {
   return (
