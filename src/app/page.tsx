1cf27887215f40b19ec6fb4e49f632916177c35b
Cesar Emmanuel <cesaremmmanul@gmail.com>
2025-05-31 15:01:28 -0300

    feat: SEO home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import LandingPage from '@/templates/landing-page';
+import type { Metadata } from 'next';
+
+export const metadata = {
+  title: 'Site.Set',
+  description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: 'https://next-pages-blog-ecru.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://next-pages-blog-ecru.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set - Venda seus produtos como afiliado em um único lugar',
+      },
+    ],
+  },
+} satisfies Metadata;
 
 function HomePage() {
   return <LandingPage />;
